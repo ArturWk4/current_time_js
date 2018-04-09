e4dac493356999c4c84ec93f21cce57946ef05d8
ArturWk4 <elkin.as@mail.ru>
2018-04-09 20:21:15 +0300

    Base implementation of check current time, at future time I'll add beautiful design for it

---
+++ time.js
@@ -0,0 +1,17 @@
+function printTime() {
+	let currentDate = new Date();
+	let hours = currentDate.getHours();
+	let minutes = currentDate.getMinutes();
+	let seconds = currentDate.getSeconds();
+		if(seconds < 10){
+			seconds = "0" + seconds;
+		}
+		if(minutes < 10){
+			minutes = "0" + minutes;
+		}
+		if(hours < 10){
+			hours = "0" + hours;
+		}
+		document.body.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+setInterval(printTime, 1000);
